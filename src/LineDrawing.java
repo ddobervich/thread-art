1fdd4dcaf3907373214e4d36abad3c85eca9e505
David Dobervich <david.dobervich@gmail.com>
2022-06-09 15:54:36 -0700

    remove unused stuff

--- src/LineDrawing.java
+++ src/LineDrawing.java
@@ -11,89 +11,22 @@ public class LineDrawing implements PixelFilter, Drawable, Interactive {
     private ArrayList<Point> pegs;
     private ArrayList<Point> additionalPoints;
     private ArrayList<Line> lines;
-    private int bestIndex = 0;
-    private double[][] blurKernel =
-            {
-                    {0.003879, 0.003993, 0.004091, 0.004174, 0.004239, 0.004287, 0.004315, 0.004325, 0.004315, 0.004287, 0.004239, 0.004174, 0.004091, 0.003993, 0.003879},
-                    {0.003993, 0.004109, 0.004211, 0.004296, 0.004363, 0.004412, 0.004442, 0.004452, 0.004442, 0.004412, 0.004363, 0.004296, 0.004211, 0.004109, 0.003993},
-                    {0.004091, 0.004211, 0.004315, 0.004402, 0.004471, 0.004521, 0.004552, 0.004562, 0.004552, 0.004521, 0.004471, 0.004402, 0.004315, 0.004211, 0.004091},
-                    {0.004174, 0.004296, 0.004402, 0.004491, 0.004562, 0.004613, 0.004644, 0.004654, 0.004644, 0.004613, 0.004562, 0.004491, 0.004402, 0.004296, 0.004174},
-                    {0.004239, 0.004363, 0.004471, 0.004562, 0.004633, 0.004685, 0.004716, 0.004727, 0.004716, 0.004685, 0.004633, 0.004562, 0.004471, 0.004363, 0.004239},
-                    {0.004287, 0.004412, 0.004521, 0.004613, 0.004685, 0.004737, 0.004769, 0.00478, 0.004769, 0.004737, 0.004685, 0.004613, 0.004521, 0.004412, 0.004287},
-                    {0.004315, 0.004442, 0.004552, 0.004644, 0.004716, 0.004769, 0.004801, 0.004811, 0.004801, 0.004769, 0.004716, 0.004644, 0.004552, 0.004442, 0.004315},
-                    {0.004325, 0.004452, 0.004562, 0.004654, 0.004727, 0.00478, 0.004811, 0.004822, 0.004811, 0.00478, 0.004727, 0.004654, 0.004562, 0.004452, 0.004325},
-                    {0.004315, 0.004442, 0.004552, 0.004644, 0.004716, 0.004769, 0.004801, 0.004811, 0.004801, 0.004769, 0.004716, 0.004644, 0.004552, 0.004442, 0.004315},
-                    {0.004287, 0.004412, 0.004521, 0.004613, 0.004685, 0.004737, 0.004769, 0.00478, 0.004769, 0.004737, 0.004685, 0.004613, 0.004521, 0.004412, 0.004287},
-                    {0.004239, 0.004363, 0.004471, 0.004562, 0.004633, 0.004685, 0.004716, 0.004727, 0.004716, 0.004685, 0.004633, 0.004562, 0.004471, 0.004363, 0.004239},
-                    {0.004174, 0.004296, 0.004402, 0.004491, 0.004562, 0.004613, 0.004644, 0.004654, 0.004644, 0.004613, 0.004562, 0.004491, 0.004402, 0.004296, 0.004174},
-                    {0.004091, 0.004211, 0.004315, 0.004402, 0.004471, 0.004521, 0.004552, 0.004562, 0.004552, 0.004521, 0.004471, 0.004402, 0.004315, 0.004211, 0.004091},
-                    {0.003993, 0.004109, 0.004211, 0.004296, 0.004363, 0.004412, 0.004442, 0.004452, 0.004442, 0.004412, 0.004363, 0.004296, 0.004211, 0.004109, 0.003993},
-                    {0.003879, 0.003993, 0.004091, 0.004174, 0.004239, 0.004287, 0.004315, 0.004325, 0.004315, 0.004287, 0.004239, 0.004174, 0.004091, 0.003993, 0.003879}
-            };
-
-    public LineDrawing() {
-        this.pegs = new ArrayList<>();
-        this.additionalPoints = new ArrayList<>();
-    }
 
     @Override
     public DImage processImage(DImage img) {
         short[][] bwpixels = img.getBWPixelGrid();
-        //short[][] bluredPixels = blur(bwpixels, blurKernel);
+
         short[][] bluredPixels = new short[1][1];
         if (pegs.size() < NUM_PEGS) {
             initPegs(pegs, bwpixels, NUM_PEGS);
-            //initSquareGrid(pegs, bwpixels, 50);
-            //initRandomPegs(pegs, bwpixels, NUM_PEGS);
         }
 
         lines = generateLines(pegs, bwpixels, bluredPixels);
         System.out.println("Lines generated!");
-        //bestIndex = findOptimalNumberOfLines(lines, img.getBWPixelGrid());
-        //System.out.println("Best index is: " + bestIndex + " out of " + lines.size());
 
         fill(bwpixels, (short) 255);
         img.setPixels(bwpixels);
         return img;
-    }
-
-    public short[][] blur(short[][] pixels, double[][] kernel) {
-        int halfSize = kernel.length / 2;
-        short[][] output = new short[pixels.length][pixels[0].length];
-
-        for (int r = 0; r < pixels.length - kernel.length; r++) {
-            for (int c = 0; c < pixels.length - kernel[r].length; c++) {
-                short outPixel = applyKernel(pixels, r, c, kernel);
-                output[r + halfSize][c + halfSize] = outPixel;
-            }
-        }
-
-        return output;
-    }
-
-    private short applyKernel(short[][] pixels, int r, int c, double[][] kernel) {
-        double pixelVal = 0;
-
-        for (int i = 0; i < kernel.length; i++) {
-            for (int j = 0; j < kernel[i].length; j++) {
-                double weight = kernel[i][j];
-                short val = pixels[r + i][c + i];
-                pixelVal += weight * val;
-            }
-        }
-
-        if (pixelVal > 255) return 255;
-        if (pixelVal < 0) return 0;
-        return (short) pixelVal;
-    }
-
-    private void initRandomPegs(ArrayList<Point> pegs, short[][] bwpixels, int numPegs) {
-        for (int i = 0; i < numPegs; i++) {
-            int row = (int) (Math.random() * bwpixels.length);
-            int col = (int) (Math.random() * bwpixels[0].length);
-            Point peg = new Point(row, col);
-            pegs.add(peg);
-        }
     }
 
     private void fill(short[][] bwpixels, short color) {
@@ -165,24 +98,6 @@ public class LineDrawing implements PixelFilter, Drawable, Interactive {
         }
 
         return new Line(currentPeg, bestNext);
-    }
-
-    public int findOptimalNumberOfLines(List<Line> lines, short[][] pixels) {
-        int bestNum = 0;
-        double bestAverageDarkness = Double.MAX_VALUE;
-
-        double totalBrightnessSoFar = 0;
-        for (int i = 0; i < lines.size(); i++) {
-            Line currentLine = lines.get(i);
-            totalBrightnessSoFar = brightnessOfPath(currentLine.p1, currentLine.p2, pixels);
-            double average = totalBrightnessSoFar / (i + 1);
-            if (average < bestAverageDarkness) {
-                bestAverageDarkness = average;
-                bestNum = i;
-            }
-        }
-
-        return bestNum;
     }
 
     private double brightnessOfPath(Point currentPeg, Point nextPeg, short[][] pixels) {
